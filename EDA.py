1339f5b834e3d453e268ab39aeab4498cce35115
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2024-02-28 20:41:37 +0800

    change default file

--- EDA.py
+++ EDA.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # Set default file path
-DEFAULT_FILE_PATH = "Orders.xlsx"
+DEFAULT_FILE_PATH = "US_MarketSTock.csv"
 
 # Columns to advise user to remove
 COLUMNS_TO_REMOVE = ["id", "date"]
